f32b11f9ad3cd8c0ff9e591ce8cde844117b956c
Jarle <jarle.mathiesen@gmail.com>
2021-09-11 17:34:03 +0200

    fix install script

--- install.sh
+++ install.sh
@@ -5,7 +5,8 @@ set -e
 SSL_REPO="git@github.com:jarle/dotfiles.git"
 HTTPS_REPO="https://github.com/jarle/dotfiles.git"
 
-TARGET="$HOME/git/dotfiles"
+GIT_DIR="$HOME/git"
+TARGET="$GIT_DIR/dotfiles/"
 
 
 function main() {
@@ -15,10 +16,11 @@ function main() {
 }
 
 function clone() {
-    mkdir -p $TARGET
+    mkdir -p "$GIT_DIR"
     if [[ -f "$TARGET"/dot ]]; then
         (cd $TARGET && git pull --rebase)
     else
+        (cd "$GIT_DIR")
         git clone --depth=1 $SSL_REPO $TARGET || echo "Unable to install with SSL keys, using HTTPS" && \
             git clone --depth=1 $HTTP_REPO $TARGET
     fi
